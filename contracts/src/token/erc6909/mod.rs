d09f3e58cc9805c8376aafc05c9312d2f77ddb2f
Samuel Rojo Álvarez <39249425+samuelrojoalvarez@users.noreply.github.com>
2025-07-07 04:34:51 +0200

    Update mod.rs

--- contracts/src/token/erc6909/mod.rs
+++ contracts/src/token/erc6909/mod.rs
@@ -1,25 +1,3 @@
-// #![no_std]
-
-// pub mod erc6909;
-// pub mod error;
-// pub mod extensions;
-// pub mod traits;
-
-// // ─── Core exports
-// // ───────────────────────────────────────────────────────────────
-// pub use erc6909::Erc6909;
-// pub use error::Error;
-// pub use extensions::metadata_uri::Erc6909MetadataUri;
-// // ─── Extensions (types)
-// // ─────────────────────────────────────────────────────────
-// pub use extensions::token_supply::Erc6909Supply;
-// // ─── Traits export
-// // ──────────────────────────────────────────────────────────────
-// pub use traits::{
-//     IErc6909, IErc6909Burnable, IErc6909MetadataUri, IErc6909Mintable,
-//     IErc6909Supply,
-// };
-
 #![no_std]
 
 pub mod erc6909;
